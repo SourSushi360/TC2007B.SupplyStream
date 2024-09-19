e4602302ed8e1b1f67f245ed8dcc5b92aa62848f
SourSushi360 <jplopez365@hotmail.com>
2024-09-19 17:23:52 -0600

    finally made Camera View Work

--- app/(tabs)/camera.jsx
+++ app/(tabs)/camera.jsx
@@ -1,13 +1,39 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Camera } from 'expo-camera';
+import { CameraView as ExpoCameraView, useCameraPermissions } from 'expo-camera';
+import { useState } from 'react';
+import { Button, Text, TouchableOpacity, View } from 'react-native';
 
 const CameraView = () => {
-  return (
-    <View>
-      <Text>CameraView</Text>
-    </View>
-  )
-}
+  const [facing, setFacing] = useState('back');
+  const [permission, requestPermission] = useCameraPermissions();
 
-export default CameraView
+  if (!permission) {
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    return (
+      <View className="flex-1 justify-center">
+        <Text className="text-center pb-2">We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="Grant Permission" />
+      </View>
+    );
+  }
+
+  function toggleCameraFacing() {
+    setFacing(current => (current === 'back' ? 'front' : 'back'));
+  }
+
+  return (
+    <View className="flex-1">
+      <ExpoCameraView className="flex-1" style={{ facing }}>
+        <View className="flex-1 flex-row bg-transparent m-16">
+          <TouchableOpacity className="flex-1 self-end items-center" onPress={toggleCameraFacing}>
+            <Text className="text-2xl font-bold text-white">Flip Camera</Text>
+          </TouchableOpacity>
+        </View>
+      </ExpoCameraView>
+    </View>
+  );
+};
+
+export default CameraView;
