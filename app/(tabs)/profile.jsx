57a863a44b8d6658f2da59d5729bd599485923f1
SourSushi360 <jplopez365@hotmail.com>
2024-09-19 16:34:21 -0600

    corrected profile view

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,11 +1,33 @@
-import { Text, View, Image } from 'react-native'
+import { Text, View, Image, Alert } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { useNavigation } from '@react-navigation/native' // Importa el hook de navegación
 
 import { images } from '../../constants';
 import CustomButton from '../../components/CustomButton';
 
 const Profile = () => {
+  const navigation = useNavigation();
+
+  const handleDeleteAccount = () => {
+    Alert.alert(
+      "Eliminar Cuenta",
+      "¿Estás seguro de que deseas eliminar tu cuenta?",
+      [
+        {
+          text: "Cancelar",
+          style: "cancel"
+        },
+        { text: "Eliminar", onPress: () => console.log("Cuenta eliminada") } // handle deletion when connected to db
+      ],
+      { cancelable: false }
+    );
+  };
+
+  const handleLogout = () => {
+    navigation.navigate('index');
+  };
+
   return (
     <SafeAreaView>
       <View className="items-center mt-8">
@@ -19,14 +41,14 @@ const Profile = () => {
           <Text className="ml-2 text-xl font-plight">Administración</Text>
         </View>
         <CustomButton 
-          title="Cerrar Sesión"
-          handlePress={() => {}}
+          title="Eliminar Cuenta"
+          handlePress={handleDeleteAccount}
           containerStyles="mt-12 w-[200px]"
           color='bg-red'
         />
         <CustomButton 
-          title="Eliminar Cuenta"
-          handlePress={() => {}}
+          title="Cerrar Sesión"
+          handlePress={handleLogout}
           containerStyles="mt-12 w-[200px]"
           color='bg-red'
         />
